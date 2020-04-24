35a44e7d979b428de99375d985e39d173b4e648e
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-24 23:54:21 +0100

    Create 13-Interpreter-v1.swift

---
+++ 13WaysOfLookingAtATurtle/13-Interpreter-v1.swift
@@ -0,0 +1,9 @@
+//
+//  13-Interpreter-v1.swift
+//  13WaysOfLookingAtATurtle
+//
+//  Created by Christian Leovido on 24/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import Foundation
