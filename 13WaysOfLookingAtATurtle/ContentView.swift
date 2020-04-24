99ff3a697a6c87f8bbf32b923f15afe6db15fcb3
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-24 23:53:35 +0100

    initial files

---
+++ 13WaysOfLookingAtATurtle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  13WaysOfLookingAtATurtle
+//
+//  Created by Christian Leovido on 24/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
