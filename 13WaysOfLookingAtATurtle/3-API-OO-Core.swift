99eb0f7e2ad5482f118dff571df258bcfd7e8f1f
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-24 23:53:46 +0100

    Create 3-API-OO-Core.swift

---
+++ 13WaysOfLookingAtATurtle/3-API-OO-Core.swift
@@ -0,0 +1,9 @@
+//
+//  3-API-OO-Core.swift
+//  13WaysOfLookingAtATurtle
+//
+//  Created by Christian Leovido on 24/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import Foundation
