c950427c39dacc25ad182f7c5745c7da5751da50
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-25 21:42:49 +0100

    Delete 6-DependencyInjection-Functions-2.swift

--- 13WaysOfLookingAtATurtle/6-DependencyInjection-Functions-2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  6-DependencyInjection-Functions-2.swift
-//  13WaysOfLookingAtATurtle
-//
-//  Created by Christian Leovido on 24/04/2020.
-//  Copyright © 2020 Christian Leovido. All rights reserved.
-//
-
-import Foundation
