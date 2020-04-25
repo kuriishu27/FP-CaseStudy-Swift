f30271acb0ccac8c75e1872ce9d9288a6fe1abd2
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-25 21:42:29 +0100

    Create 8-StateMonads.swift

---
+++ 13WaysOfLookingAtATurtle/8-StateMonads.swift
@@ -0,0 +1,9 @@
+//
+//  7-StateMonads.swift
+//  13WaysOfLookingAtATurtle
+//
+//  Created by Christian Leovido on 24/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import Foundation
